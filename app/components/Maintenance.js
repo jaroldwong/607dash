324b0a99bcfca71412a4c561e0522f969fcef091
Jarold Wong <jaroldw@gmail.com>
2017-05-14 10:11:44 -0700

    Display maintenance issues

--- app/components/Maintenance.js
+++ app/components/Maintenance.js
@@ -1,11 +1,48 @@
 var React = require('react')
 
 class Maintenance extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            issues: [
+                {
+                    name: "Martin Loekito",
+                    issue: "Flickering light in the staff suite",
+                    status: "New",
+                },
+                {
+                  name: "Jun Hwang",
+                  issue: "Leaking soap dispenser",
+                  status: "In progress",
+                }
+            ],
+        };
+    }
+    
     render() {
         return (
             <div>
-              Maintenance
+              <h2>Issues</h2>
+              <MaintenanceList issues={this.state.issues} />
             </div>
+        )
+    }
+}
+
+class MaintenanceList extends React.Component {
+    render() {
+        return (
+            <ul className="maintenance-list">
+              {this.props.issues.map(function (issue, index) {
+                return (
+                  <li className="maintenance-item">
+                    <p>Status: {issue.status}</p>
+                    <p>Issue: {issue.issue}</p>
+                    <p>Submitted by: {issue.name}</p>                
+                  </li>
+                )
+              })}
+            </ul>
         )
     }
 }
